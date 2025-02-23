e7a034acd1418b1481066195ca3bfeba9bee9d1a
Nola Gerold <gingerwitch64@gmail.com>
2025-02-23 15:32:56 -0800

    prob05 done

--- 2023/prob05.py
+++ 2023/prob05.py
@@ -1,5 +1,11 @@
 import sys
 string: str = sys.stdin.read().rstrip()
+output: str = ""
 
 for ch in string:
     ascii_code: int = ord(ch)
+    top_digit: int    = ascii_code // 36
+    middle_digit: int = (ascii_code - (top_digit * 36)) // 6
+    bottom_digit: int = (ascii_code - (top_digit * 36) - (middle_digit * 6)) // 1
+    output = output + f"{top_digit}{middle_digit}{bottom_digit}"
+print(output)
